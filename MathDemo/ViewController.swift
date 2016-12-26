77de473732e245f44f76555b2f3f12898ff04162
iOS Student <iosstudent@techmasters-imac.local>
2016-12-26 16:21:04 +0700

    Initial Commit

---
+++ MathDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathDemo
+//
+//  Created by iOS Student on 12/26/16.
+//  Copyright © 2016 tek4fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
